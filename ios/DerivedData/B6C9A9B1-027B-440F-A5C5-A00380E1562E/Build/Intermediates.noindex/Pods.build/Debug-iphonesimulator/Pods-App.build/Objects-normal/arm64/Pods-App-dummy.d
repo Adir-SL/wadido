172d9bb9fac9784793b646a56515d0c6ea6a2f4a
Adir <slutzkis@gmail.com>
2024-09-22 14:14:01 +0300

    Deleted all

--- ios/DerivedData/B6C9A9B1-027B-440F-A5C5-A00380E1562E/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-App.build/Objects-normal/arm64/Pods-App-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/adirsl/Documents/GitHub/wadido/ios/DerivedData/B6C9A9B1-027B-440F-A5C5-A00380E1562E/Build/Intermediates.noindex/Pods.build/Debug-iphonesimulator/Pods-App.build/module.modulemap \
-  /Users/adirsl/Documents/GitHub/wadido/ios/App/Pods/Target\ Support\ Files/Pods-App/Pods-App-dummy.m \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/Foundation.framework/Modules/module.modulemap
